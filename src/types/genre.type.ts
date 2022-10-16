8ad157120fa803837d67e33633389998ec18c570
Сергей Луговых <lugowyh.serezha@yandex.ru>
2022-10-17 01:46:19 +0500

    remove 'as' from genre

--- src/types/genre.type.ts
+++ src/types/genre.type.ts
@@ -14,9 +14,13 @@ const genresStrings: readonly string[] = genres;
 
 export type Genre = typeof genres[number];
 
+function isGenre(s: string): s is Genre {
+  return genresStrings.includes(s);
+}
+
 export function asGenre(s: string): Genre {
-  if (genresStrings.includes(s)) {
-    return s as Genre;
+  if (!isGenre(s)) {
+    throw new Error(`No genre ${s}`);
   }
-  throw new Error(`No genre ${s}`);
+  return s;
 }
