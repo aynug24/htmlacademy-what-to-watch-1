db36ec2af85795c7666251f660879a2660c9fb66
Сергей Луговых <lugowyh.serezha@yandex.ru>
2022-11-21 02:50:00 +0500

    add mongo services

---
+++ src/modules/comment/comment-service.interface.ts
@@ -0,0 +1,9 @@
+import {DocumentType} from '@typegoose/typegoose/lib/types.js';
+import {CommentEntity} from './comment.entity.js';
+import CreateCommentDto from './dto/create-comment.dto.js';
+
+export interface ICommentService {
+  create(dto: CreateCommentDto): Promise<DocumentType<CommentEntity>>;
+  findByMovieId(offerId: string): Promise<DocumentType<CommentEntity>[]>;
+  deleteByMovieId(offerId: string): Promise<number | null>;
+}
--- src/modules/movie/movie-service.interface.ts
+++ src/modules/movie/movie-service.interface.ts
@@ -1,8 +1,15 @@
 import {DocumentType} from '@typegoose/typegoose';
 import {MovieEntity} from './movie.entity.js';
 import CreateMovieDto from './dto/create-movie.dto.js';
+import UpdateMovieDto from './dto/update-movie.dto.js';
+import {Genre} from '../../types/genre.type.js';
 
 export interface IMovieService {
   create(dto: CreateMovieDto): Promise<DocumentType<MovieEntity>>;
   findById(offerId: string): Promise<DocumentType<MovieEntity> | null>;
+  updateById(movieId: string, dto: UpdateMovieDto): Promise<DocumentType<MovieEntity> | null>;
+  deleteById(movieId: string): Promise<DocumentType<MovieEntity> | null>;
+  findNew(count?: number): Promise<DocumentType<MovieEntity>[]>;
+  exists(movieId: string): Promise<boolean>;
+  findByGenre(genre: Genre, count?: number): Promise<DocumentType<MovieEntity>[]>;
 }
